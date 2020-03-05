7e190a050479d66e0de76c5f78137cd63bf03fcd
joeburnard <joe.burnard@redbrain.com>
2020-03-05 20:27:19 +0000

    vandlestick writer

--- dataservice/client.go
+++ dataservice/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"github.com/ducc/kwɒnt/protos"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
@@ -12,14 +13,16 @@ var (
 )
 
 func init() {
-	flag.StringVar(&dataServiceClientAddress, "dataservice.default", "", "")
+	flag.StringVar(&dataServiceClientAddress, "dataservice-address", "dataservice.data", "")
 }
 
 func NewClient(ctx context.Context) (protos.DataServiceClient, error) {
+	logrus.WithField("address", dataServiceClientAddress).Debug("connecting to data service")
 	conn, err := grpc.DialContext(ctx, dataServiceClientAddress, grpc.WithBlock(), grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 
+	logrus.WithField("address", dataServiceClientAddress).Debug("connected to data service")
 	return protos.NewDataServiceClient(conn), nil
 }
