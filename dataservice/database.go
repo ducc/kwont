478936bde96f96d286dda5b3d5e8edf4abacb454
Joe Burnard <joe.burnard@redbrain.com>
2020-06-16 21:54:16 +0100

    pass order id as custom comment to xtb

--- dataservice/database.go
+++ dataservice/database.go
@@ -396,12 +396,12 @@ UPSERT INTO xtb_trades (session_id, "order", timestamp, close_price, close_time,
 						comment, commission, custom_comment, digits, expiration, margin_rate, 
 						"offset", open_price, open_time, order_2, position, profit, stop_loss, 
 						state, storage, symbol, take_profit, type, volume)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, 
+VALUES ($1, $2, $3, $4, $5, $6::bool, $7, $8, $9, $10, 
 		$11, $12, $13, $14, $15, $16, $17, $18, $19, 
 		$20, $21, $22, $23, $24, $25, $26);
 `
 
-	if _, err := d.db.ExecContext(ctx, stmt, sessionID, order, timestamp, closePrice, closeTime, sql.NullBool{Bool: closed, Valid: true}, cmd,
+	if _, err := d.db.ExecContext(ctx, stmt, sessionID, order, timestamp, closePrice, closeTime, closed, cmd,
 		comment, commission, customComment, digits, expiration, marginRate,
 		offset, openPrice, openTime, order2, position, profit, stopLoss,
 		state, storage, symbol, takeProfit, tradeType, volume); err != nil {
